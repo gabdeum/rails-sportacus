c58c555edd61804dd02ffa6caa48e45a94b3affb
Gabriel Demange <gabriel.demange@gmail.com>
2022-09-09 13:52:48 -0400

    Started home page html/css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  include Pundit::Authorization
+
+  # Pundit: allow-list approach
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   protected
 
@@ -9,6 +14,10 @@ class ApplicationController < ActionController::Base
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :description])
+  end
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:account]
 
   def home
+    # authorize @articles
+    @articles = Article.all
   end
 
   def account
